1de046fd2d2b2f8729385f7762d14320530d949b
2crazytroll <nikolazmarriaga@gmail.com>
2026-03-13 15:55:52 -0500

    udapte project with laster version

--- sales.py
+++ sales.py
@@ -1,10 +1,24 @@
 sales_list = []
 
 def register_sale():
+    
+    #name validation
+    val1=1
+    while val1 ==1 :
+        product_name = str(input("Enter product name: "))
+        if not product_name.isalpha():
+                print("ERROR: ")
+                continue
 
-    product_name = input("Enter product name: ")
+        val1 +=2    
+
+    #price validation
     product_price = float(input("Enter product price: "))
+
+    #quantity validetion
     product_quantity = int(input("Enter quantity sold: "))
+
+
 
     sale = {
         "product": product_name,
@@ -15,4 +29,5 @@ def register_sale():
     sales_list.append(sale)
 
     print("Sale registered successfully")
-    
+
+
